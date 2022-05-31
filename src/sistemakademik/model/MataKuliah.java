96a859690b52658cb49fb6661f2f797519ccaad7
didik <didikunila@gmail.com>
2022-05-31 15:08:10 +0700

    Edit MakaKuliah.java

--- src/sistemakademik/model/MataKuliah.java
+++ src/sistemakademik/model/MataKuliah.java
@@ -10,5 +10,38 @@ package sistemakademik.model;
  * @author didik
  */
 public class MataKuliah {
+    private String kodeMK;
+    private String namaMK;
+    private int sks;
+
+    public MataKuliah(String kodeMK, String namaMK, int sks) {
+        this.kodeMK = kodeMK;
+        this.namaMK = namaMK;
+        this.sks = sks;
+    }
+
+    public int getSks() {
+        return sks;
+    }
+
+    public void setSks(int sks) {
+        this.sks = sks;
+    }
+
+    public String getKodeMK() {
+        return kodeMK;
+    }
+
+    public void setKodeMK(String kodeMK) {
+        this.kodeMK = kodeMK;
+    }
+
+    public String getNamaMK() {
+        return namaMK;
+    }
+
+    public void setNamaMK(String namaMK) {
+        this.namaMK = namaMK;
+    }
     
 }
